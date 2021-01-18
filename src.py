9910be968eb429b94263e85c03d7d89f424b1b80
Praneet <praneetquantumx@gmail.com>
2021-01-18 13:04:51 +0530

    Update src.py

--- src.py
+++ src.py
@@ -6,25 +6,35 @@ from tqdm import tqdm
 
 
 class Dataset:
-    def __init__(self, input_path, target_path):
+    def __init__(self, input_path=None, target_path=None, save=False, load_x=None, load_y=None):
         self.input_path = input_path
         self.target_path = target_path
         self.X = []
         self.Y = []
+        self.save = save
+        self.x_path = load_x
+        self.y_path = load_y
         self.make_synthetic_dataset()
 
     def make_synthetic_dataset(self):
-        for img_name in tqdm(os.listdir(self.input_path)):
-            img = cv2.imread(os.path.join(self.input_path, img_name))
-            img = cv2.resize(img, (100, 100))
-            img = np.array(img) / 255
-            self.X.append(img)
-        for img_name in tqdm(os.listdir(self.target_path)):
-            img = cv2.imread(os.path.join(self.target_path, img_name))
-            img = cv2.resize(img, (100, 100))
-            img = np.array(img) / 255
-            self.Y.append(img)
-        assert len(self.X) == len(self.Y), "Err: src target mismatch. Dataset not clean"
+        if self.x_path is None and self.y_path is None and self.input_path is not None and self.target_path is not None:
+            for img_name in tqdm(os.listdir(self.input_path)):
+                img = cv2.imread(os.path.join(self.input_path, img_name))
+                img = cv2.resize(img, (100, 100))
+                img = np.array(img) / 255
+                self.X.append(img)
+            for img_name in tqdm(os.listdir(self.target_path)):
+                img = cv2.imread(os.path.join(self.target_path, img_name))
+                img = cv2.resize(img, (100, 100))
+                img = np.array(img) / 255
+                self.Y.append(img)
+            assert len(self.X) == len(self.Y), "Err: src target mismatch. Dataset not clean"
+            if self.save:
+                np.save('x-dataset', self.X)
+                np.save('y-dataset', self.Y)
+        elif self.x_path is not None and self.y_path is not None:
+            self.X = np.load(self.x_path)
+            self.Y = np.load(self.y_path)
 
 
 class AutoEncoder:
